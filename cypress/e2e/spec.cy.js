3e592b4af8a2cfeec3fcbf986921ee7ab14ad120
Tinagitdv <kuinatheauri@gmail.com>
2025-03-11 12:51:48 +0000

    Fix report and flakyness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,27 @@
+import { LoginData } from "./pages/login/login.data";
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random01';
+    const password = 'random01';
+    
+    cy.visit('https://demoblaze.com');
+    cy.get('a[data-target="#logInModal"]').click();
+
+    LoginMethods.login(usuario, password);
+    cy.get('a#nameofuser').should('contain.text', usuario);
+
+    cy.wait(5000);
+
+    /*const usuario = 'random01';
+    const password = 'random01';
+    cy.visit('https://demoblaze.com');
+
+
+    cy.get('a[data-target="#logInModal"]').click();
+
+    LoginMethods.login('username', 'password');
+    cy.wait(5000);*/
   })
 })

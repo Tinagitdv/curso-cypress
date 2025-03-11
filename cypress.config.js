4f63c5a4ea89e186683abe281a7b4299a1aa126d
Tinagitdv <kuinatheauri@gmail.com>
2025-03-11 12:51:22 +0000

    Fix report and flakyness

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+//const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //allureWriter(on, config);
+      configureAllureAdapterPlugins(on, config);
+      return config;
     },
   },
 });

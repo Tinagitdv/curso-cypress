83b16eb422939ffee8a44668695908967fb84781
Tinagitdv <kuinatheauri@gmail.com>
2025-03-12 15:30:02 +0000

    Creación de archivos de HomePage

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,19 @@
+import { HomeMethods } from "./pages/home/home.methods";
 import { LoginData } from "./pages/login/login.data";
 import { LoginMethods } from "./pages/login/login.methods";
 
 describe('template spec', () => {
   it('passes', () => {
-    const usuario = 'random01';
+
+  cy.visit('https://demoblaze.com/index.html');
+    HomeMethods.clickOnProductLink("Iphone 6 32gb");
+    cy.wait(5000);
+
+
+
+
+    /**********************************/
+    /*const usuario = 'random01';
     const password = 'random01';
     
     cy.visit('https://demoblaze.com');
@@ -12,7 +22,7 @@ describe('template spec', () => {
     LoginMethods.login(usuario, password);
     cy.get('a#nameofuser').should('contain.text', usuario);
 
-    cy.wait(5000);
+    cy.wait(5000);*/
 
     /*const usuario = 'random01';
     const password = 'random01';
